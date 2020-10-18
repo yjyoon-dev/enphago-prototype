fbb50da808d158722f47575a686299bc8589ccaf
unknown <yjyoon.dev@gmail.com>
2020-10-18 23:27:24 +0900

    final proto type

--- turn_test.py
+++ turn_test.py
@@ -5,16 +5,44 @@ from two_pronoun_rules import convert
 
 wordList = []
 usedList = []
+learningList = []
 lastWord = ""
 
 # 단어 리스트 로드
 def wordInit():
-    f = open('word_list.txt','r')
+    try:
+        f = open('word_list.txt','r')
+    except FileNotFoundError:
+        print('단어 리스트를 로드할 수 없습니다.')
+        exit(0)
+
     while True:
         line = f.readline().strip()
         if not line: break
         wordList.append(line)
+
+    f.close()
+
     random.shuffle(wordList)
+
+
+# 학습 단어 리스트 로드
+def learningInit():
+    try:
+        f = open('learning_list.txt','r')
+        
+    except FileNotFoundError:
+        print('학습 리스트를 로드할 수 없습니다.')
+        exit(0)
+
+    while True:
+        line = f.readline().strip()
+        if not line: break
+        learningList.append(line)
+
+    f.close()
+
+    random.shuffle(learningList)
 
 
 # 한글표준어대사전 Open API
@@ -76,8 +104,8 @@ def searchWord(word):
 
 # 끝말 일치 확인
 def isMatched(first,later):
-    rear = list(first)[-1]
-    front = list(later)[0]
+    rear = first[-1]
+    front = later[0]
 
     rear = convert(rear) # 두음법칙 변환
 
@@ -86,13 +114,35 @@ def isMatched(first,later):
 
 # AI 단어 탐색
 def getAIWord(myWord):
+    # 학습 리스트 탐색
+    for word in learningList:
+        if isMatched(myWord,word) and word not in usedList:
+            return word
+    
+    # 단어 리스트 탐색
     for word in wordList:
         if isMatched(myWord,word) and word not in usedList:
             return word
 
 
+# 단어 리스트 추가
+def addWord(word):
+    f = open("word_list.txt", 'a')
+    f.write(word+'\n')
+    f.close()
+
+
+# AI 단어 학습
+def learnWord(word):
+    if word not in learningList:
+        f = open("learning_list.txt", 'a')
+        f.write(word+'\n')
+        f.close()
+
+
 # 게임 진행
 wordInit()
+learningInit()
 while True:
     print()
     myWord = input("[내 차례] : ").strip()
@@ -100,9 +150,14 @@ while True:
     # GG
     if myWord == "/기권":
         print('패배하셨습니다.')
+
+        # hint 출력
         hint = getAIWord(lastWord)
         if hint is not None:
             print('*Hint : '+hint)
+        
+        learnWord(lastWord) # 단어 학습
+
         break
     
     # 공백 입력 시
@@ -132,12 +187,17 @@ while True:
     # 플레이어 단어 사용 반영
     usedList.append(myWord)
     
+    # 플레이어 단어 학습
+    if myWord not in wordList:
+        addWord(myWord)
+
     # AI 단어 불러오기
     aiWord = getAIWord(myWord)
 
     # 실패 시 플레이어 승리
     if aiWord is None:
         print('당신의 승리!')
+        learnWord(myWord)
         break
 
     # AI 단어 사용 반영
